8732a182d98109a6512b219a28148d5e9d263028
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2024-07-17 17:07:02 -0300

    estudo de hookform

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Formulario1 from "./components/formulario1";
+
+function App() {
+  return (
+    <>
+      <Formulario1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/formulario1.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+let contadorPerform = 0;
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
+const Formulario1: React.FC = () => {
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+  //   const { name, ref, onChange, onBlur } = register("username");
+  //   posso usar assim por desestruturação ou ....
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Click no botão", data);
+  };
+
+  contadorPerform++;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-xs">
+        <p>Exemplo de performance ({contadorPerform / 2})</p>
+        <form
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label className="block text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+              {...register("username", { required: "Username obrigatório" })}
+              //   register ja vai me dar todos os atributos anteriores e com isso eu posso editar só o necessario
+              //   name={name}
+              //   ref={ref}
+              //   onChange={onChange}
+              //   onBlur={onBlur}
+            />
+          </div>
+          <p className="text-red-500">{errors.username?.message}</p>
+          <div className="mb-4">
+            <label className="block text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Email invalido",
+                },
+              })}
+            />
+          </div>
+          <p className="text-red-500">{errors.email?.message}</p>
+          <div className="mb-6">
+            <label className="block text-sm font-bold mb-2" htmlFor="channel">
+              Channel
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="channel"
+              type="text"
+              placeholder="Channel"
+              {...register("channel", {
+                minLength: 3,
+                required: {
+                  value: true,
+                  message: "Channel precisa ter pelo menos 3 caracteres",
+                },
+              })}
+            />
+          </div>
+          <p className="text-red-500">{errors.channel?.message}</p>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        <DevTool control={control} />
+      </div>
+    </div>
+  );
+};
+
+export default Formulario1;
